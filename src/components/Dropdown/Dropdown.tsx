0bc142056d9aee787f059cfac5f134ffacd49918
lisa_ywl <yourworthlesslife@icloud.com>
2023-09-04 13:15:10 +0300

    added dropdown logic for showing 2 options and an arrow rotation

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,29 +1,66 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Dropdown.module.css"
-// import { Arrow } from "../Arrow";
+import { Arrow } from "../Arrow";
 
 export function Dropdown(props: any) {
   const [value, setValue] = useState("halfyear");
-  // const [isActive, setIsActive] = useState(false);
+  const [hiddenOptions, setHiddenOptions] = useState<string[]>([]);
+  // для стрелки
+  const [isActive, setIsActive] = useState(false);
 
-  // const handleClick = () => {
-  //   setIsActive(current => !current);
-  // }
+  const handleToggleActive = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleChange = (selectedValue: string) => {
+    // Убираю предыдушие options из массива со скрытыми
+    setHiddenOptions((prevHiddenOptions) =>
+      prevHiddenOptions.filter((option) => option !== value)
+    );
+
+    setValue(selectedValue);
+
+    // Выбранная option в массив со скрытыми
+    setHiddenOptions((prevHiddenOptions) => [...prevHiddenOptions, selectedValue]);
+
+    props.childToParent(selectedValue);
+  };
+
+  // Скрываею выбранную опцию на загрузку компонента
+  useEffect(() => {
+    handleChange(value);
+  }, []);
+
+  const options = [
+    { value: "halfyear", label: "За последние 6 месяцев" },
+    { value: "month", label: "За последний месяц" },
+    { value: "year", label: "За последний год" },
+  ];
+
 
   return (
     <div className={styles.dropdown}>
-      <select value={value}
-        // className={isActive ? `${styles.select} arrow_rotate` : `${styles.select}`} onClick={handleClick}
+      <select value={value} id="select"
         className={styles.select}
         onChange={(e) => {
-          setValue(e.target.value);
-          props.childToParent(e.target.value);
-        }}>
-        <option value={"halfyear"}>За последние 6 месяцев</option>
-        <option value={"month"}>За последний месяц</option>
-        <option value={"year"}>За последний год</option>
+          handleChange(e.target.value);
+        }}
+        onClick={handleToggleActive}
+        onBlur={handleToggleActive}
+      >
+        {options.map((option) => (
+          <option
+            key={option.value}
+            value={option.value}
+            hidden={hiddenOptions.includes(option.value)}
+            className={styles.option}
+          >
+            {option.label}
+          </option>
+        ))}
       </select>
-    </div>
+      <Arrow className={`${styles.arrow} ${isActive ? styles.active : ''}`} />
+    </div >
   )
 }
